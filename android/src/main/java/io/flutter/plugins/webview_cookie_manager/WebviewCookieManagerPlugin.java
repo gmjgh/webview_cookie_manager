4c78c737bbe9d7253a98b683b4a9906f50c26cb4
paulostasha <paul.ostasha@ridewithvia.com>
2025-05-01 12:40:26 +0300

    Deleted registrar

--- android/src/main/java/io/flutter/plugins/webview_cookie_manager/WebviewCookieManagerPlugin.java
+++ android/src/main/java/io/flutter/plugins/webview_cookie_manager/WebviewCookieManagerPlugin.java
@@ -20,7 +20,6 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
 
 /**
  * WebviewCookieManagerPlugin
@@ -41,11 +40,6 @@ public class WebviewCookieManagerPlugin implements FlutterPlugin, MethodCallHand
     // them functionally equivalent. Only one of onAttachedToEngine or registerWith will be called
     // depending on the user's project. onAttachedToEngine or registerWith must both be defined
     // in the same class.
-    public static void registerWith(Registrar registrar) {
-        final MethodChannel channel = new MethodChannel(registrar.messenger(), "webview_cookie_manager");
-        channel.setMethodCallHandler(new WebviewCookieManagerPlugin());
-    }
-
     private static void hasCookies(final Result result) {
         CookieManager cookieManager = CookieManager.getInstance();
         final boolean hasCookies = cookieManager.hasCookies();
